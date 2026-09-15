0bae8f2f5e113070fbfb96de7bd4c2028ccf7028
rehanjirayat-hub <rehanjirayat@gmail.com>
2026-09-15 06:33:24 -0700

    Complete core domain model

--- src/test/java/model/ResponseTeamTest.java
+++ src/test/java/model/ResponseTeamTest.java
@@ -60,4 +60,37 @@ public class ResponseTeamTest {
         assertTrue(responseTeam.getCapabilities()
                 .contains(ResourceCapability.FIRE_RESPONSE));
     }
+
+    @Test
+    void teamShouldStoreCurrentAssignment(){
+
+
+        Incident incident = new Incident(
+                "Medical emergency",
+                Severity.CRITICAL,
+                new Location(12.9716, 77.5946)
+        );
+
+        EmergencyResource resource = new EmergencyResource(
+                1,
+                ResourceType.AMBULANCE,
+                new Location(12.9716, 77.5946)
+        );
+
+        ResponseTeam responseTeam = new ResponseTeam(1);
+
+        Dispatch dispatch = new Dispatch(
+                1,
+                incident,
+                resource,
+                responseTeam
+        );
+
+        responseTeam.getCurrentAssignments().add(dispatch);
+
+        assertTrue(responseTeam.getCurrentAssignments().contains(dispatch));
+
+
+
+    }
 }
